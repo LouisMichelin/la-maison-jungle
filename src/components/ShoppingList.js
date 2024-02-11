87ef784df8d73c18828c45d574809d4056aab09a
LouisMichelin <louis.michelin097@gmail.com>
2024-02-11 16:23:17 +0100

    Upload partie "props"

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,33 +1,34 @@
-import { plantList } from "../datas/plantList"
-import "../styles/ShoppingList.css"
+import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-
-  const categories = plantList.reduce(
+	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
-      []
+		[]
 	)
 
-
-  return (
-    <div>
-      <ul>
-        {categories.map((cat) => (
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
-      </ul>
-      <ul>
-        {plantList.map((plant) => (
-					<li key={plant.id} className="each-plant">
-            {plant.name}
-            {(plant.isBestSale || plant.category === "classique") && <span>🔥</span>}
-            {plant.isSpecialOffer && <div className="lmj-sales">Soldes</div>}
-          </li>
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
-      </ul>
-    </div>
-  )
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
